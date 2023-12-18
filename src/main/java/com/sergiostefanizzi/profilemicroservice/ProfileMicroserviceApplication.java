b2de6155870b466763997b62d78b6e5c7229693b
Sergio Stefanizzi <sergio.stef25@gmail.com>
2023-12-18 11:23:57 +0100

    lang

--- src/main/java/com/sergiostefanizzi/profilemicroservice/ProfileMicroserviceApplication.java
+++ src/main/java/com/sergiostefanizzi/profilemicroservice/ProfileMicroserviceApplication.java
@@ -3,12 +3,6 @@ package com.sergiostefanizzi.profilemicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
-
 @SpringBootApplication
 public class ProfileMicroserviceApplication {
 
@@ -16,11 +10,6 @@ public class ProfileMicroserviceApplication {
 		SpringApplication.run(ProfileMicroserviceApplication.class, args);
 	}
 
-	@Bean
-	public LocaleResolver localeResolver() {
-		SessionLocaleResolver slr = new SessionLocaleResolver();
-		slr.setDefaultLocale(Locale.ENGLISH);
-		return slr;
-	}
+
 
 }
---
+++ src/main/java/com/sergiostefanizzi/profilemicroservice/system/util/LanguageConfig.java
@@ -0,0 +1,18 @@
+package com.sergiostefanizzi.profilemicroservice.system.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class LanguageConfig {
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.ENGLISH);
+        return slr;
+    }
+}
